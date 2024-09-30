a5103d23ee4124c590ba934e78e322c1f6628efa
Benjamin Esquieres <40147170+delta-12@users.noreply.github.com>
2024-09-29 22:06:10 -0400

    NNF-98 csv parser (#15)
    * NNF-11 BE added code
    
    * NNF-13 DLP added code (#8)
    
    * NNF-17 SV added code
    
    * NNF-14 MWI added code (#7)
    
    Moaz's squash and merge
    
    * NNF-15 NA added code (#10)
    
    * Nnf 16 rory cooke GitHub branch (#11)
    
    * NNF-16 RC added code
    
    * NNF-16 RC added code v2
    
    * Create charles_prather.cc (#12)
    
    * NNF-94 add docker container
    
    * NNF-94 add launcher demo
    
    * NNF-94 set style to cosmic-dark
    
    * NNF-94 README quick start instructions
    
    * NNF-94 remove initial commits
    
    * NNF-94 structure modules
    
    * NNF-94 structure short-term module testing
    
    * NNF-94 add module sources to trains build
    
    * NNF-94 track and train model interfaces
    
    * NNF-94 declare controller gateways
    
    * NNF-94 re-arrange architecture
    
    * NNF-94 simulator
    
    * NNF-98-CSV-Parser basic csv parser
    
    * NNF-94 implement simulator
    
    * NNF-94 build UIs with temporary module tests
    
    * NNF-98 make Split static
    
    * NNF-98 parse by file path and stringstream
    
    ---------
    
    Co-authored-by: Dominique Pantin <106163992+dlp80@users.noreply.github.com>
    Co-authored-by: Son Vu <truongson.vu72@gmail.com>
    Co-authored-by: Moaz796 <122473319+Moaz796@users.noreply.github.com>
    Co-authored-by: Nasser Abdulwahab <122320807+ChibiDemon@users.noreply.github.com>
    Co-authored-by: roryy19 <133978648+roryy19@users.noreply.github.com>
    Co-authored-by: Cjp103 <122712478+Cjp103@users.noreply.github.com>

---
+++ src/common/src/csv_parser.cc
@@ -0,0 +1,125 @@
+#include "csv_parser.h"
+
+#include <fstream>
+
+#define CSVPARSER_VALUE_DELIMITER ","
+#define CSVPARSER_ROW_DELIMITER   "\n"
+
+static std::vector<std::string> Split(const std::string &input_string, const std::string &delimiter);
+
+CsvParser::CsvParser(void)
+{
+}
+
+CsvParser::CsvParser(const std::filesystem::path &file_path)
+{
+    Parse(file_path);
+}
+
+CsvParser::CsvParser(const std::stringstream &string_stream)
+{
+    Parse(string_stream);
+}
+
+CsvParser::CsvParser(const std::string &input_string)
+{
+    Parse(input_string);
+}
+
+bool CsvParser::Parse(const std::filesystem::path &file_path)
+{
+    bool parsed = false;
+
+    std::ifstream file(file_path);
+
+    if (file.is_open())
+    {
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        file.close();
+
+        parsed = Parse(buffer);
+    }
+
+    return parsed;
+}
+
+bool CsvParser::Parse(const std::stringstream &string_stream)
+{
+    return Parse(string_stream.str());
+}
+
+bool CsvParser::Parse(const std::string &input_string)
+{
+    bool parsed = true;
+
+    Reset();
+
+    std::vector<std::string> lines = Split(input_string, CSVPARSER_ROW_DELIMITER);
+
+    for (const std::string &line : lines)
+    {
+        records_.push_back(Split(line, CSVPARSER_VALUE_DELIMITER));
+    }
+
+    const std::size_t size = records_[0].size();
+    for (const std::vector<std::string> &record : records_)
+    {
+        if (record.size() != size)
+        {
+            parsed = false;
+        }
+    }
+
+    if (!parsed)
+    {
+        Reset();
+    }
+
+    return parsed;
+}
+
+std::size_t CsvParser::GetSize(void) const
+{
+    return records_.size();
+}
+
+std::vector<std::string> CsvParser::GetRecord(const std::size_t index) const
+{
+    std::vector<std::string> record;
+
+    if (index < records_.size())
+    {
+        record = records_[index];
+    }
+
+    return record;
+}
+
+std::vector<std::vector<std::string> > CsvParser::GetRecords(void) const
+{
+    return records_;
+}
+
+void CsvParser::Reset(void)
+{
+    records_.clear();
+}
+
+static std::vector<std::string> Split(const std::string &input_string, const std::string &delimiter)
+{
+    std::vector<std::string> tokens;
+    size_t                   index = 0;
+    size_t                   end   = input_string.find(delimiter, index);
+
+    while (std::string::npos != end)
+    {
+        tokens.push_back(input_string.substr(index, end - index));
+
+        index = end + 1;
+        end   = input_string.find(delimiter, index);
+    }
+    tokens.push_back(input_string.substr(index, input_string.length() - index));
+
+    return tokens;
+}
