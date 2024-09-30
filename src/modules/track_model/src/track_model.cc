3c6394b12e30ed96f79531b62219a47a56f962a1
Moaz <MWI1@pitt.edu>
2024-09-30 15:16:44 -0400

    first commit

--- src/modules/track_model/src/track_model.cc
+++ src/modules/track_model/src/track_model.cc
@@ -1,1 +1,11 @@
 #include "track_model.h"
+
+namespace track_model
+{
+
+void SoftwareTrackModel::TestMethod(void) const {
+    //IMPLEMENT
+
+    //cmake --build build -t target
+}
+} // namespace track_model
