2ccba2154d73ec193cda4cc72c97e4f8e884e4d3
dlp80 <dlp80@pitt.edu>
2024-10-03 19:38:35 -0400

    NNF-142 outlined classes and structures used by WC backend

--- src/modules/wayside_controller/inc/wayside_controller.h
+++ src/modules/wayside_controller/inc/wayside_controller.h
@@ -7,3 +7,86 @@
 #define TRAINS_SRC_MODULES_WAYSIDE_CONTROLLER_INC_WAYSIDE_CONTROLLER_H
 
 #endif // TRAINS_SRC_MODULES_WAYSIDE_CONTROLLER_INC_WAYSIDE_CONTROLLER_H
+
+#include <vector>
+
+class WaysideController
+{
+    public:
+    //primary variables
+    bool switch_state, crossing_state, signal_light;
+    double commanded_speed;
+    int authority;
+
+    //identifiers
+    int switch_id, crossing_id, signal_id;
+
+    //incoming from CTC
+    float CTC_suggestedspd;
+    std::vector<int, int> CTC_authority;
+
+    //base level functions
+    WaysideController();
+    void setSwitch(bool);
+    void setCrossing(bool);
+    void setSignal(bool);
+
+    void getSwitch(bool);
+    void getCrossing(bool);
+    void getSignal(bool);
+
+    void getTrackFailures(bool);
+
+
+
+    //block information
+    struct Block{
+        bool occupancy;
+        bool failure_state;
+        //int block number;
+
+    }
+
+
+};
+
+class PLC
+{
+public:
+    
+    /*struct PLCpacket{
+
+
+    }*/
+
+   //plc processing functions
+    void PLCget(int[]);
+    void PLCset(int[]);
+
+};
+
+
+
+
+
+
+
+/*
+        TrackController();
+        void setUpController(int, std::string &, std::vector<char> &, std::vector<int> &, int[], int, int[]);
+        void setSignalsInstance(CTCSignals &);
+        void setSwitch(bool);
+        void setSwitchAuto();
+        void addBlockObj(int);
+        void setTrackSA();
+        void setRoute();
+		int getResult();
+        void setCross();
+        void manSetCross(bool);
+        void getFaults();
+        void getOccupancies();
+        void updateData();
+};
+
+#endif /*TrackController_h*/
+*/
