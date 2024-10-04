a2f759a51d5c6987e6096066de2cfecdf9a753ab
Charles Prather <cjp103@pitt.edu>
2024-10-04 15:52:17 -0400

    Added mostly fleshed out UI with some backend

--- src/modules/train_model/src/train_model.cc
+++ src/modules/train_model/src/train_model.cc
@@ -1,1 +1,34 @@
 #include "train_model.h"
+
+int main()
+{
+    auto ui = TrainModel::create();
+
+    ui->on_request_ebrake([&]{
+        if (ui->get_ebrake_status() == "PASS")
+            ui->set_ebrake_status("FAIL");
+        else
+            ui->set_ebrake_status("PASS");
+    });
+    ui->on_request_engine_fail([&]{
+        if (ui->get_engine_status() == "PASS")
+            ui->set_engine_status("FAIL");
+        else
+            ui->set_engine_status("PASS");
+    });
+    ui->on_request_signal_fail([&]{
+        if (ui->get_signal_status() == "PASS")
+            ui->set_signal_status("FAIL");
+        else
+            ui->set_signal_status("PASS");
+    });
+    ui->on_request_brake_fail([&]{
+        if (ui->get_brake_status() == "PASS")
+            ui->set_brake_status("FAIL");
+        else
+            ui->set_brake_status("PASS");
+    });
+
+    ui->run();
+    return 0;
+}
