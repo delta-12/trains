d7023e6d9a1fe35643355b276dda8b0803e57d62
Benjamin Esquieres <40147170+delta-12@users.noreply.github.com>
2024-10-03 22:42:51 -0400

    NNF 97 Tick Source (#17)
    * NNF-97 invalid format type
    
    * NNF-97 implement tick source
    
    * NNF-97 fix pre-commit hook
    
    * NNF-97 enable unit testing
    
    * NNF-97 initialize, multiplier and duration tests
    
    * NNF-97 get elapsed time test
    
    * NNF-97 set time test
    
    * NNF-97 test get time string in gui
    
    * NNF-97 ensure compatability with windows when getting time point from string
    
    * NNF-97 re-enable SetTime test

--- tests/modules/wayside_controller_tests_ui.cc
+++ tests/modules/wayside_controller_tests_ui.cc
@@ -4,13 +4,21 @@
  * @brief Short-term testing for the Wayside Controller backend.
  *****************************************************************************/
 
+#include "tick_source.h"
 #include "wayside_controller.h"
 #include "wayside_controller_ui.h"
 
 int main(void)
 {
     auto wayside_controller_ui = ui::WaysideControllerUi::create();
+    TickSource tick_source("12:00:00");
+    wayside_controller_ui->set_time(tick_source.GetTimeString().c_str());
 
+    wayside_controller_ui->on_get_time([&]{
+        wayside_controller_ui->set_time(tick_source.GetTimeString().c_str());
+    });
+
+    tick_source.Start();
     wayside_controller_ui->run();
 
     return 0;
