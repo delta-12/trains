c3e8e81cb04736d1ba67557aff0fb6f8bd340af4
roryy19 <rcookie011@gmail.com>
2024-10-05 18:14:21 -0400

    NNF-123 added operation mode status alert with 3 second timer

--- tests/modules/train_controller_tests_ui.cc
+++ tests/modules/train_controller_tests_ui.cc
@@ -6,20 +6,38 @@
 
 #include "train_controller.h"
 #include "train_controller_ui.h"
+#include <chrono>
+#include <memory>
+#include <slint_timer.h>
+#include <thread>
 
 int main(void)
 {
     auto train_controller_ui = ui::TrainControllerUi::create();
 
+    // operation mode
+
     train_controller_ui->on_request_automatic_mode([&]{
         if (train_controller_ui->get_operation_mode_status() == "Manual")
-            train_controller_ui->set_operation_mode_status("In Manual, cannot switch to Automatic");
+        {
+            train_controller_ui->set_operation_status_message("In Manual, cannot switch to Automatic");
+
+            // timer to clear the message after 3 seconds
+            auto timer = std::make_shared<slint::Timer>();
+            timer->single_shot(std::chrono::seconds(3), [train_controller_ui, timer]() {
+                train_controller_ui->set_operation_status_message("");
+            });
+        }
     });
     train_controller_ui->on_request_manual_mode([&]{
         train_controller_ui->set_operation_mode_status("Manual");
         });
 
     train_controller_ui->run();
+
+    // failures
+
+    //train_controller_ui->on
 
     return 0;
 }
