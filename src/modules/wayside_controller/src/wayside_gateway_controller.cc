c02ed74504011c6f18ed60c8db68d65f96b95e0c
delta-12 <btesq235@gmail.com>
2024-10-05 14:17:33 -0400

    NNF-160 add ticket numbers to wayside gateway TODOs

--- src/modules/wayside_controller/src/wayside_gateway_controller.cc
+++ src/modules/wayside_controller/src/wayside_gateway_controller.cc
@@ -9,45 +9,49 @@ void Gateway::UpdateControllers(void)
 
 types::Error Gateway::UpdateTrackModel(std::shared_ptr<track_model::TrackModel> track)
 {
-    // TODO receive data
+    // TODO NNF-172 receive data
 }
 
 types::Error Gateway::UpdateTrackModels(std::vector<std::shared_ptr<track_model::TrackModel> > &tracks)
 {
+    // TODO NNF-172
 }
 
 void Gateway::SetSimulationMode(const bool simulation)
 {
-    // TODO
+    // TODO NNF-172
 }
 
 bool Gateway::GetSimulationMode(void) const
 {
-    // TODO
+    // TODO NNF-172
 
     return false;
 }
 
 void Gateway::AttachPort(std::shared_ptr<types::Port> port)
 {
+    // TODO NNF-172
 }
 
 types::Error Gateway::SetSuggestedSpeedAndAuthority(const types::TrackId track, const types::BlockId block, const types::MetersPerSecond speed, const types::Meters authority)
 {
-    // TODO serialize data, lookup wayside corresponding to track and block, send data
+    // TODO NNF-172 serialize data, lookup wayside corresponding to track and block, send data
 }
 
 types::Error Gateway::SetMaintenanceMode(const types::TrackId track, const types::BlockId block, const bool maintenance)
 {
+    // TODO NNF-172
 }
 
 types::Error Gateway::SetSwitchState(const types::TrackId track, const types::BlockId block, const bool switch_state)
 {
+    // TODO NNF-172
 }
 
 void Gateway::GetBlockStates(const types::TrackId track, std::vector<BlockState> &states)
 {
-    // TODO
+    // TODO NNF-172
 }
 
 
