845fd2ded3f371740247365672de21bd417ae526
Moaz <MWI1@pitt.edu>
2024-10-05 14:56:21 -0400

    Block occupancy mostly done, began building UI

--- src/modules/track_model/inc/track_model.h
+++ src/modules/track_model/inc/track_model.h
@@ -27,14 +27,77 @@ class TrackModel
         virtual types::Error SetRedTrafficLight(const types::BlockId block, const bool on)                        = 0;
         virtual types::Error SetYelloTrafficLight(const types::BlockId block, const bool on)                      = 0;
         virtual types::Error SetGreenTrafficLight(const types::BlockId block, const bool on)                      = 0;
-        virtual types::Error SetCommandedSpeed(const types::BlockId block, const types::MetersPerSecond speed)    = 0;
-        virtual types::Error SetAuthority(const types::BlockId block, const types::Meters authority)              = 0;
+        virtual types::Error SetCommandedSpeed(const types::BlockId block, const types::MetersPerSecond speed)    = 0; //other modules
+        virtual types::Error SetAuthority(const types::BlockId block, const types::Meters authority)              = 0; //other modules
         virtual types::Error GetBlockOccupancy(const types::BlockId block, bool &occupied) const                  = 0;
         virtual types::Error SetBrokenRail(const types::BlockId block, const bool broken)                         = 0;
         virtual types::Error SetTrackCircuitFailure(const types::BlockId block, const bool track_circuit_failure) = 0;
         virtual types::Error SetPowerFailure(const types::BlockId block, const bool power_failure)                = 0;
+        virtual types::Error SetExternalTemperature(const types::DegreesFahrenheit temperature)                   = 0;
 };
 
+class SoftwareTrackModel : public TrackModel {
+    public:
+        SoftwareTrackModel();
+        virtual types::TrackId GetTrackId(void);
+        virtual types::Error AddTrainModel(std::shared_ptr<train_model::TrainModel> train);
+        virtual void GetTrainModels(std::vector<std::shared_ptr<train_model::TrainModel> > &trains) const;
+        virtual void Update(void);
+        virtual types::Error SetSwitchState(const types::BlockId block, const bool switched);
+        virtual types::Error SetCrossingState(const types::BlockId block, const bool closed);
+        virtual types::Error SetRedTrafficLight(const types::BlockId block, const bool on);
+        virtual types::Error SetYelloTrafficLight(const types::BlockId block, const bool on);
+        virtual types::Error SetGreenTrafficLight(const types::BlockId block, const bool on);
+        virtual types::Error SetCommandedSpeed(const types::BlockId block, const types::MetersPerSecond speed);
+        virtual types::Error SetAuthority(const types::BlockId block, const types::Meters authority);
+        virtual types::Error GetBlockOccupancy(const types::BlockId block, bool &occupied) const;
+        virtual types::Error SetBrokenRail(const types::BlockId block, const bool broken);
+        virtual types::Error SetTrackCircuitFailure(const types::BlockId block, const bool track_circuit_failure);
+        virtual types::Error SetPowerFailure(const types::BlockId block, const bool power_failure);
+        virtual types::Error SetExternalTemperature(const types::DegreesFahrenheit temperature);
+
+    private:
+        types::TrackId track_id;
+        std::vector<std::shared_ptr<train_model::TrainModel>> trainsvec;
+        std::vector<types::BlockId> currblock={1};
+        std::vector<std::vector<types::BlockId>> blockvisited;
+        std::vector<uint16_t> tpassengers;
+        int temp;
+
+        struct Block
+        {
+            bool occupancy=0;
+            std::vector<int> switches;
+            bool crossing=0;
+            bool light=0;
+            std::string lightcolor="GREEN";
+            bool pfail=0;
+            bool tcfail=0;
+            bool brail=0;
+            int grade=0;
+            types::Meters length=50;
+            int slimit=50;
+            int elevation=0;
+            char section='A';
+            bool heater=0;
+            char station='Z';
+            int direction=0;
+            types::BlockId connection;
+        
+        };
+
+        //initial vector with all blocks loaded from the csv (think of this as a database)
+        std::vector<Block> allblocks;
+
+        //vector containing current actual line with swtiches and branches factored in
+        std::vector<Block> line;
+
+        int maxpass=222;
+
+        std::vector<std::shared_ptr<train_model::TrainModel>> trainmodels;
+
+        types::Meters tlength=32;
+};
 } // namespace track_model
 
 #endif // TRAINS_SRC_MODULES_TRACK_MODEL_INC_TRACK_MODEL_H
--- src/modules/track_model/src/track_model.cc
+++ src/modules/track_model/src/track_model.cc
@@ -1,1 +1,258 @@
 #include "track_model.h"
+#include "train_model.h"
+
+namespace track_model
+{
+//BLUE LINE CONSTRUCTOR
+track_model::SoftwareTrackModel::SoftwareTrackModel()
+{
+    track_id = 1;
+    std::vector<Block> newstuff = {
+        Block(),
+        Block(),
+        Block(),
+        Block(),
+        Block(),
+        Block(),
+        Block(),
+        Block(),
+        Block(),
+        Block(),
+        Block(),
+        Block(),
+        Block(),
+        Block(),
+        Block(),
+        Block()
+    };
+
+    newstuff[2].crossing = 1;
+    newstuff[4].switches.push_back(6);
+    newstuff[4].switches.push_back(11);
+    newstuff[5].light    = 1;
+    newstuff[9].station  = 'B';
+    newstuff[10].light   = 1;
+    newstuff[14].station = 'C';
+
+    for (int i = 5; i < 10; i++)
+    {
+        newstuff[i].section = 'B';
+    }
+
+    for (int i = 11; i < 15; i++)
+    {
+        newstuff[i].section = 'C';
+    }
+
+    line.insert(line.end(), newstuff.begin(), newstuff.end());
+}
+
+types::TrackId SoftwareTrackModel::GetTrackId(void)
+{
+    // Implementation logic to get the Track ID
+    return types::TrackId{ };
+}
+
+types::Error SoftwareTrackModel::AddTrainModel(std::shared_ptr<train_model::TrainModel> train)
+{
+    // Implementation logic to add a train model
+
+    //TODO: ADD FAILURE STATES
+
+    //add model
+    trainmodels.push_back(train);
+    currblock.push_back(1);
+
+    return types::ERROR_NONE;
+}
+
+
+void SoftwareTrackModel::GetTrainModels(std::vector<std::shared_ptr<train_model::TrainModel> > &trains) const
+{
+    // Populating the trains vector with current train models
+    for (int i = 0; i < trainmodels.size(); i++)
+    {
+        trains.push_back(trainmodels[i]);
+    }
+}
+
+
+void SoftwareTrackModel::Update(void)
+{
+    // Update the state of the track model
+
+    //loop through all trains on the line
+    for (int i = 0; i < trainmodels.size(); i++)
+    {
+        //get the BlockId of the train
+        types::BlockId trainblock;
+
+        //set block occupancy
+        line[trainblock - 1].occupancy = 1;
+
+        //calculate which blocks the train is currently taking up
+        //check the length of the block the train is occupying, and see if the train is longer
+        if (line[trainblock - 1].length < tlength)
+        {
+            //looping until the full length of the train is accounted for
+            auto sizeofblocks = line[trainblock - 1].length;
+            int  j            = 1;
+            while (sizeofblocks < tlength)
+            {
+                //adding the size of the block behind it
+                sizeofblocks += line[trainblock - 1 - j].length;
+
+                //adding this block to the vector of occupancies
+                line[trainblock - 1 - j].occupancy = 1;
+
+                //increment
+                j++;
+            }
+        }
+
+        //get the passengers deboarding
+        uint16_t deboard = trainmodels[i]->GetPassengersDeboarding();
+
+        //subtract from total passengers
+        tpassengers[i] -= deboard;
+        uint16_t vacancy = maxpass - tpassengers[i];
+
+        //generate random number within bounds for boarding
+        srand((unsigned)time(0));
+        uint16_t board;
+        board = (rand() % vacancy);
+
+        //set passengers boarding
+        trainmodels[i]->SetPassengersBoarding(board);
+    }
+
+
+}
+
+
+types::Error SoftwareTrackModel::SetSwitchState(const types::BlockId block, const bool switched)
+{
+    // Logic to set the switch state for the specified block
+    return types::Error{ };
+}
+
+types::Error SoftwareTrackModel::SetCrossingState(const types::BlockId block, const bool closed)
+{
+    // Logic to set the crossing state for the specified block
+    return types::Error{ };
+}
+
+types::Error SoftwareTrackModel::SetRedTrafficLight(const types::BlockId block, const bool on)
+{
+    // Logic to set the red traffic light state for the specified block
+    return types::Error{ };
+}
+
+types::Error SoftwareTrackModel::SetYelloTrafficLight(const types::BlockId block, const bool on)
+{
+    // Logic to set the yellow traffic light state for the specified block
+    return types::Error{ };
+}
+
+types::Error SoftwareTrackModel::SetGreenTrafficLight(const types::BlockId block, const bool on)
+{
+    // Logic to set the green traffic light state for the specified block
+    return types::Error{ };
+}
+
+types::Error SoftwareTrackModel::SetCommandedSpeed(const types::BlockId block, const types::MetersPerSecond speed)
+{
+    // Logic to set the commanded speed
+    return types::Error{ };
+}
+
+types::Error SoftwareTrackModel::SetAuthority(const types::BlockId block, const types::Meters authority)
+{
+    // Logic to set authority
+    return types::Error{  };
+}
+
+types::Error SoftwareTrackModel::GetBlockOccupancy(const types::BlockId block, bool &occupied) const
+{
+    // Logic to determine block occupancy and set the occupied variable
+
+    //checking if block exists
+    if (line.size() < block)
+    {
+        return types::ERROR_INVALID_BLOCK;
+    }
+
+    //check occupancy
+    if (line[block - 1].occupancy == 1)
+    {
+        //set occupancy variable
+        occupied = 1;
+    }
+    else
+    {
+        occupied = 0;
+    }
+
+    return types::ERROR_NONE;
+}
+
+types::Error SoftwareTrackModel::SetBrokenRail(const types::BlockId block, const bool broken)
+{
+    // Logic to set the broken rail state for the specified block
+
+    //checking if block exists
+    if (line.size() < block)
+    {
+        return (types::ERROR_INVALID_BLOCK);
+    }
+
+    //setting broken rail
+    line[block - 1].brail = broken;
+
+    //occupancy
+    line[block - 1].occupancy = 1;
+
+    return types::ERROR_NONE;
+}
+
+types::Error SoftwareTrackModel::SetTrackCircuitFailure(const types::BlockId block, const bool track_circuit_failure)
+{
+    // Logic to set the track circuit failure state for the specified block
+
+    //checking if block exists
+    if (line.size() < block)
+    {
+        return (types::ERROR_INVALID_BLOCK);
+    }
+
+    //setting TC fail
+    line[block - 1].tcfail = track_circuit_failure;
+
+    return types::ERROR_NONE;
+}
+
+types::Error SoftwareTrackModel::SetPowerFailure(const types::BlockId block, const bool power_failure)
+{
+    // Logic to set the power failure state for the specified block
+
+    //checking if block exists
+    if (line.size() < block)
+    {
+        return (types::ERROR_INVALID_BLOCK);
+    }
+
+    //setting TC fail
+    line[block - 1].pfail = power_failure;
+
+    //occupancy
+    line[block - 1].occupancy = 1;
+
+    return types::ERROR_NONE;
+}
+
+types::Error SoftwareTrackModel::SetExternalTemperature(const types::DegreesFahrenheit temperature)
+{
+    return types::ERROR_NONE;
+}
+
+} // namespace track_model
