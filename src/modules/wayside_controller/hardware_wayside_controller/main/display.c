00b7598ccb8d9d3de86d6dc1b5a4518920da0a89
delta-12 <btesq235@gmail.com>
2024-10-07 18:50:32 -0400

    NNF-171 select block

--- src/modules/wayside_controller/hardware_wayside_controller/main/display.c
+++ src/modules/wayside_controller/hardware_wayside_controller/main/display.c
@@ -32,6 +32,7 @@ static const char *TAG = "OLED";
 #define EXAMPLE_LCD_PARAM_BITS 8
 
 static lv_disp_t *disp = NULL;
+static lv_obj_t *label = NULL;
 
 /* The LVGL port component calls esp_lcd_panel_draw_bitmap API for send data to the screen. There must be called
 lvgl_port_flush_ready(disp) after each transaction to display. The best way is to use on_color_trans_done
@@ -106,17 +107,18 @@ void DisplayInit(void)
 
     /* Rotation of the screen */
     lv_disp_set_rotation(disp, LV_DISP_ROT_180);
+
+    lv_obj_t *scr = lv_disp_get_scr_act(disp);
+    label = lv_label_create(scr);
 }
 
 void DisplayWriteString(const char *const str)
 {
-    if (NULL != disp)
+    if ((NULL != disp) && (NULL != label) && (NULL != str))
     {
         // Lock the mutex due to the LVGL APIs are not thread-safe
         if (lvgl_port_lock(0))
         {
-            lv_obj_t *scr = lv_disp_get_scr_act(disp);
-            lv_obj_t *label = lv_label_create(scr);
             lv_label_set_long_mode(label, LV_LABEL_LONG_WRAP); /* Circular scroll */
             lv_label_set_text(label, str);
             /* Size of the screen (if you use rotation 90 or 270, please set disp->driver->ver_res) */
--- src/modules/wayside_controller/hardware_wayside_controller/main/hardware_wayside_controller.cc
+++ src/modules/wayside_controller/hardware_wayside_controller/main/hardware_wayside_controller.cc
@@ -3,22 +3,35 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#include "gpio_esp.h"
-
+#include "convert.h"
 #include "display.h"
+#include "gpio_esp.h"
+#include "types.h"
+
+#define DISPLAY_TEXT_BUFFER_SIZE 60
+#define LEFT_BUTTON 23
+#define RIGHT_BUTTON 32
+#define MIN_BLOCK_NUMBER 1
+#define MAX_BLOCK_NUMBER 16
+
+static char display_text_buffer[DISPLAY_TEXT_BUFFER_SIZE];
+static const char *block_text_format_string = "Block: %d\n"
+                                              "Commanded Speed: %dmph\n"
+                                              "Authority: %dft";
+static const char *block_text_format_string_no_command = "Block: %d\n"
+                                                         "Commanded Speed: --mph\n"
+                                                         "Authority: --ft";
+static types::BlockId selected_block = MIN_BLOCK_NUMBER;
 
 static void ButtonCallback(const bsp::GpioPin pin);
-
-static const char *test_block = "Block: 1\n"
-                                "Commanded Speed: 6mph\n"
-                                "Authority: 100m";
+static void DisplayBlock(const types::BlockId block, const types::MetersPerSecond commanded_speed, const types::Meters authority);
 
 extern "C" void app_main(void)
 {
     printf("foobar\n");
 
     DisplayInit();
-    DisplayWriteString(test_block);
+    DisplayBlock(selected_block, 20, 150);
 
     bsp_esp::EspGpioHandler gpio_handler;
 
@@ -59,16 +72,46 @@ extern "C" void app_main(void)
     gpio_handler.SetLevel(27, bsp::GPIOLEVEL_LOW);
     gpio_handler.SetLevel(26, bsp::GPIOLEVEL_LOW);
     gpio_handler.SetLevel(25, bsp::GPIOLEVEL_LOW);
+
+    DisplayBlock(selected_block, 15, -10);
 }
 
 static void ButtonCallback(const bsp::GpioPin pin)
 {
-    if (23 == pin)
+    if ((LEFT_BUTTON == pin) && (selected_block > MIN_BLOCK_NUMBER))
     {
         printf("Left button\n");
+        selected_block--;
     }
-    else if (32 == pin)
+    if ((RIGHT_BUTTON == pin) && (selected_block < MAX_BLOCK_NUMBER))
     {
         printf("Right button\n");
+        selected_block++;
     }
+}
+
+static void DisplayBlock(const types::BlockId block, const types::MetersPerSecond commanded_speed, const types::Meters authority)
+{
+    uint32_t converted_speed = (uint32_t)convert::MetersPerSecondToMilesPerHour(commanded_speed);
+    uint32_t converted_authority = (uint32_t)convert::MetersToFeet(authority);
+
+    if (commanded_speed < 0)
+    {
+        converted_speed = 0;
+    }
+    if (authority < 0)
+    {
+        converted_authority = 0;
+    }
+
+    if ((commanded_speed > 0.0) || (authority > 0.0))
+    {
+        snprintf(display_text_buffer, DISPLAY_TEXT_BUFFER_SIZE, block_text_format_string, block, converted_speed, converted_authority);
+    }
+    else
+    {
+        snprintf(display_text_buffer, DISPLAY_TEXT_BUFFER_SIZE, block_text_format_string_no_command, block);
+    }
+
+    DisplayWriteString(display_text_buffer);
 }
