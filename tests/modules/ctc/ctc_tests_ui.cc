aff74eef6c4413867825edc84daf92e5045de636
Son Vu <truongson.vu72@gmail.com>
2024-10-10 10:55:35 -0400

    NNF-100 Last Successful Build

--- tests/modules/ctc/ctc_tests_ui.cc
+++ tests/modules/ctc/ctc_tests_ui.cc
@@ -29,7 +29,7 @@ int main(void)
     auto ctc_ui = ui::CtcUi::create();
     std::string input_file_path = "";
 
-    ctc_ui->on_choose_file([&]() {
+    ctc_ui->on_choose_file([&]{
 #ifdef WIN32
         OPENFILENAME ofn;       // Common dialog box structure
         char file_name[MAX_PATH]; // Buffer for the file name
@@ -61,47 +61,54 @@ int main(void)
 #endif
     });
 
-    ctc_ui->on_on_test_clicked([&]() {
-        std::cout << "Button Pressed" << std::endl;
-    });
-
-    ctc_ui->on_parse_file([&]() {
+    ctc_ui->on_parse_file([&] {
         std::cout << "Parsing File..." << std::endl;
 
         std::filesystem::path path(input_file_path);
         CsvParser parser(path);
-
+        std::string message;
         std::vector<std::vector<std::string> > records = parser.GetRecords();
         std::cout << "Schedule size: " << parser.GetSize() << std::endl;
-        ctc.PopulateDataFromCsv(records);
-        auto it = ctc.train_schedule.begin();
-        ctc_ui->set_train_1(it->first.c_str());
-        ctc_ui->set_train_1_at(it->second.begin()->second.c_str());
-        ctc.PrintAllBlockMap();
-        ctc.PrintAllTrainSchedule();
+        if (parser.GetSize() == 0) {
+            message = "No File Is Found";
+            std::cout << message << std::endl;
+        }
+        else
+        {
+            ctc.PopulateDataFromCsv(records);
+            auto it = ctc.train_schedule.begin();
+            ctc_ui->set_train_1(it->first.c_str());
+            ctc_ui->set_train_1_dst(it->second.begin()->first.c_str());
+            ctc_ui->set_train_1_at(it->second.begin()->second.c_str());
+            ctc.PrintAllBlockMap();
+            ctc.PrintAllTrainSchedule();
 
-        std::string message;
-        for (auto& entry : ctc.train_schedule) {
-            message.append(entry.first);
-            message += "\n";
-            auto dst_at = entry.second;
-            for (const auto& schedule_entry : dst_at)
-            {
-                std::string station_name = schedule_entry.first;
-                std::string arrival_time = schedule_entry.second;
-                message.append("Suggested Speed: 100 m/s\n");
-                message.append("Authority: 4000m\n");
-                message.append("Dwell Time: 3s\n");
-                message.append(station_name);
-                message.append(": ");
-                message.append(arrival_time);
-                message.append("\n");
+            for (auto& entry : ctc.train_schedule) {
+                message.append(entry.first);
+                message += "\n";
+                auto dst_at = entry.second;
+                for (const auto& schedule_entry : dst_at)
+                {
+                    std::string station_name = schedule_entry.first;
+                    std::string arrival_time = schedule_entry.second;
+                    message.append("Suggested Speed: 100 m/s\n");
+                    message.append("Authority: 4000m\n");
+                    message.append("Dwell Time: 3s\n");
+                    message.append(station_name);
+                    message.append(": ");
+                    message.append(arrival_time);
+                    message.append("\n");
+                }
             }
         }
+        std::string sspeed = "13.8888 m/s";
+        std::string authroity = "500 m";
+        ctc_ui->set_suggested_speed(sspeed.c_str());
+        ctc_ui->set_authority(authroity.c_str());
         ctc_ui->set_confirm_automatic_dispatch(message.c_str());
     });
 
-    ctc_ui->on_manual_dispatch([&]() {
+    ctc_ui->on_manual_dispatch([&] {
         std::string train_name = std::string(ctc_ui->get_train_name());
         std::string destination = std::string(ctc_ui->get_destination());
         std::string arrival_time = std::string(ctc_ui->get_arrival_time());
@@ -111,7 +118,7 @@ int main(void)
         ctc_ui->set_train_1_dst(destination.c_str());
     });
 
-    ctc_ui->on_print_occupancy_message([&]() {
+    ctc_ui->on_print_occupancy_message([&] {
         std::string block = std::string(ctc_ui->get_block_occupancy());
         std::string section = std::string(ctc_ui->get_section_occupancy());
         std::string message = "Occupancy Signal Received: Section " + section + " Block " + block;
@@ -120,7 +127,7 @@ int main(void)
         ctc_ui->set_occupancy(true);
     });
 
-    ctc_ui->on_print_output_to_wayside([&]() {
+    ctc_ui->on_print_output_to_wayside([&] {
         std::string train_name = std::string(ctc_ui->get_train_name());
         std::string authority = std::string(ctc_ui->get_authority());
         std::string dwell_time = std::string(ctc_ui->get_dwell_time());
@@ -128,21 +135,12 @@ int main(void)
         ctc_ui->set_confirm_output_message(message.c_str());
     });
 
-    ctc_ui->on_print_maintenance_message([&]() {
+    ctc_ui->on_print_maintenance_message([&] {
         std::string block = std::string(ctc_ui->get_maintenance_block());
         std::string section = std::string(ctc_ui->get_maintenance_section());
         std::string message = "Maintenance Mode Set: Section " + section + " Block " + block;
         ctc_ui->set_confirm_maintenance_message(message.c_str());
     });
-
-    
-
-    // ctc_ui->on_print_switch_message([&]() {
-    //     std::string block = std::string(ctc_ui->get_block());
-    //     std::string section = std::string(ctc_ui->get_section());
-    //     std::string message = "Switch Change Signal: Section " + section + " Block " + block;
-    //     ctc_ui->set_confirm_switch_message(message.c_str());
-    // });
 
     ctc_ui->run();
 
