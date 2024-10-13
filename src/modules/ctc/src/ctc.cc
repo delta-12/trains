62a119c61ad2e7c5247b7ed668b9cd3899fdab46
Son Vu <truongson.vu72@gmail.com>
2024-10-13 19:53:33 -0400

    NNF-100 Rework Interface, clean up ctc_tests_ui.cc and ctc.cc

--- src/modules/ctc/src/ctc.cc
+++ src/modules/ctc/src/ctc.cc
@@ -17,25 +17,6 @@
 namespace ctc
 {
 
-std::vector<std::string> SplitBySemicolon(const std::string& input)
-{
-    std::vector<std::string> result;
-    std::stringstream        ss(input);
-    std::string              token;
-
-    // Split by ';' and push each part into the result vector
-    while (std::getline(ss, token, ';'))
-    {
-        // Optionally, trim whitespace around the token
-        token.erase(0, token.find_first_not_of(" \t\n\r")); // Trim leading whitespace
-        token.erase(token.find_last_not_of(" \t\n\r") + 1); // Trim trailing whitespace
-
-        result.push_back(token);
-    }
-
-    return result;
-}
-
 } // namespace ctc
 
 #endif // TRAINS_SRC_MODULES_CTC_INC_CTC_CC
