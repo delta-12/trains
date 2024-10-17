c771bbb2c0fc53ed316a5cd0d05b70597a499142
Son Vu <truongson.vu72@gmail.com>
2024-10-16 22:27:04 -0400

    NNF-99 Backend Changes

--- src/modules/ctc/inc/ctc.h
+++ src/modules/ctc/inc/ctc.h
@@ -7,19 +7,71 @@
 #define TRAINS_SRC_MODULES_CTC_INC_CTC_H
 
 #include <unordered_map>
+#include <algorithm>
 
 #include "types.h"
 #include "wayside_controller.h"
 #include "wayside_controller_gateway.h"
 
+
+
 namespace ctc
 {
 
-typedef struct
+typedef struct DestinationAndArrivalTime DestinationAndArrivalTime;
+typedef struct Train Train;
+typedef struct Station Station;
+
+typedef enum
 {
-    types::BlockId block;
+    AUTOMATIC_MODE,
+    MANUAL_MODE,
+    MAINTENANCE_MODE
+} OperationMode;
+
+struct Station
+{
+    Station(void) : station_name(""), block_id(0) {};
+    Station(std::string station_name, types::BlockId block_id) : station_name(station_name), block_id(block_id) {};
+    std::string station_name;
+    types::BlockId block_id;
+};
+
+struct DestinationAndArrivalTime
+{
+    DestinationAndArrivalTime(void);
+    DestinationAndArrivalTime(const Station station, types::Tick arrival_time) : station(station), arrival_time(arrival_time) {};
+    ctc::Station station;
     types::Tick arrival_time;
-} DestinationAndArrivalTime;
+};
+
+// 
+struct Train
+{
+    Train(void) : train_id(++last_id) {};
+    Train(std::string train_name) : train_name(train_name), train_id(++last_id) {};
+    types::TrainId train_id;
+    types::TrackId track_id;
+    std::string train_name;
+    std::vector<types::BlockId> block_occupancy;
+    types::MetersPerSecond suggested_speed;
+    std::vector<types::Block> authority;
+    std::vector<DestinationAndArrivalTime> destination_list;
+    static types::TrainId last_id;
+};
+
+class Line
+{  
+    public:
+        Line(void);
+        Line(const std::vector<types::Block> &block_list);
+        std::unordered_map<types::BlockId, types::Block> block_map_;
+        std::vector<ctc::Station> station_list_;
+        types::TrackId track_;
+
+        types::Block GetBlock(const types::BlockId blockId );
+        Station GetStationByStationName(const std::string station_name );
+};
 
 class Ctc
 {
@@ -27,8 +79,19 @@ class Ctc
         void SetTrackLayout(const std::vector<types::Block> &blocks);
         void SetSchedule(const types::TrainId train, const std::vector<DestinationAndArrivalTime> &schedule);
         void SetManualMode(void);
+
+        void ManualDispatch(std::string &arrival_time, std::string &station_name, std::string &train_name, types::TrackId track_id);
         types::Error SetBlockStates(const types::TrackId track, const std::vector<wayside_controller::BlockState> &block_states);
         std::vector<wayside_controller::TrackCircuitData> GetSuggestedSpeedsAndAuthorities(void) const;
+
+        ctc::Train GetTrainByName(const std::string train_name);
+        ctc::Line GetLine(const types::TrackId track_id);
+
+        /* Variables */
+        std::unordered_map<types::TrackId, ctc::Line> track_layout_;
+        std::unordered_map<types::TrainId, ctc::Train> train_schedule_;
+        OperationMode ctc_mode_;
+
 };
 
 std::vector<std::string> SplitBySemicolon(const std::string& input);
--- src/modules/ctc/src/ctc.cc
+++ src/modules/ctc/src/ctc.cc
@@ -17,6 +17,104 @@
 namespace ctc
 {
 
+/*------------------------------ Line Implementation ------------------------------*/
+Line::Line(void)
+{
+}
+
+Line::Line(const std::vector<types::Block> &block_list)
+{
+    for (const types::Block &block : block_list)
+    {
+        types::Block blk = block;
+        block_map_[blk.block] = blk;
+        if (blk.has_station)
+        {
+            ctc::Station station = Station(blk.station_name, blk.block);
+            station_list_.push_back(station);
+        }
+    }
+    // Implement Line (TrackId)
+}
+
+types::Block Line::GetBlock(const types::BlockId block_id)
+{
+    types::Block block;
+    auto it = block_map_.find(block_id);
+    if (it != block_map_.end())
+    {
+        block = it->second;
+    }
+    return block;
+}
+
+Station Line::GetStationByStationName(const std::string station_name)
+{
+    ctc::Station station;
+    for (const auto &s : station_list_) {
+        if (s.station_name == station_name) {
+            station = s;
+        }
+    }
+    return station;
+}
+
+/*------------------------------ Ctc Implementation ------------------------------*/
+void Ctc::SetTrackLayout(const std::vector<types::Block> &blocks) 
+{
+    // Need Implementation
+}
+
+ctc::Line Ctc::GetLine(const types::TrackId track_id)
+{
+    auto it = track_layout_.find(track_id);
+    if (it != track_layout_.end())
+    {
+        return it->second;
+    }
+    else 
+    {
+        throw std::runtime_error("ERROR: Track ID not found in Track Layout.");
+    }
+}
+
+void Ctc::ManualDispatch(std::string &arrival_time, std::string &station_name, std::string &train_name, types::TrackId track_id) {
+    auto it = std::find_if(train_schedule_.begin(), train_schedule_.end(),
+                           [&](const auto &pair) {
+                               return pair.second.train_name == train_name;
+                           });
+    ctc::Line line_info = GetLine(track_id);
+
+    // Prepare DestinationAndArrivalTime object
+    ctc::Station station = line_info.GetStationByStationName(station_name);
+    std::string arrival_t = arrival_time;
+    types::Tick t;
+    DestinationAndArrivalTime dst(station, t);
+
+    if (it != train_schedule_.end())
+    {
+        it->second.destination_list.push_back(dst);
+    }
+    else
+    {
+        Train new_train(train_name);
+        new_train.destination_list.push_back(dst);
+        // Implement Suggested Speed and Authority
+        train_schedule_.insert({new_train.train_id, new_train});
+    }
+}
+
+ctc::Train Ctc::GetTrainByName(const std::string train_name)
+{
+    ctc::Train train;
+    for (const auto &t : train_schedule_) {
+        if (t.second.train_name == train_name) {
+            train = t.second;
+        }
+    }
+    return train;
+}
+
 } // namespace ctc
 
 #endif // TRAINS_SRC_MODULES_CTC_INC_CTC_CC
