0b2bda11891123ca385536570f227893a109d5cd
Charles Prather <cjp103@pitt.edu>
2024-10-24 16:41:46 -0400

    Updating all work made on backend and .h file for train model

--- src/modules/train_model/inc/train_model.h
+++ src/modules/train_model/inc/train_model.h
@@ -24,32 +24,73 @@ class TrainModel
         virtual void SetBrakeFailure(const bool brake_failure)                               = 0;
         virtual void SetSignalPickupFailure(const bool signal_pickup_failure)                = 0;
         virtual bool GetBrakeFailure(void) const                                             = 0;
-        virtual std::size_t GetBeaconData(types::BeaconData &data) const                     = 0;
-        virtual types::Meters GetAuthority(void) const                                       = 0;
-        virtual types::MetersPerSecond GetCommandedSpeed(void) const                         = 0;
-        virtual types::MetersPerSecond GetActualSpeed(void) const                            = 0;
         virtual bool GetEngineFailure(void) const                                            = 0;
         virtual bool GetSignalPickupFailure(void) const                                      = 0;
+        virtual std::size_t GetBeaconData(types::BeaconData &data) const                     = 0;
+        virtual types::Blocks GetAuthority(void) const                                       = 0;
+        virtual void SetCommandedSpeed(types::MilesPerHour speed)                            = 0;
+        virtual types::MilesPerHour GetCommandedSpeed(void) const                            = 0;
+        virtual types::MetersPerSecond GetActualSpeed(void) const                            = 0;
         virtual types::Watts GetActualPower(void) const                                      = 0;
+        virtual void SetActualPower(types::Watts watts)                                      = 0;
         virtual types::DegreesFahrenheit GetActualInternalTemperature(void) const            = 0;
         virtual types::Polarity GetTrackPolarity(void) const                                 = 0;
-        virtual void SetStationAnnouncement(const std::string &announcement)                 = 0;
         virtual void SetGrade(const float grade)                                             = 0;
-        virtual void SetBrake(const float brake)                                             = 0;
+        virtual void SetBrake(const double brake)                                            = 0;
+        virtual void SetStationAnnouncement(std::string announcement)                        = 0;
         virtual void SetHeadlights(const bool on)                                            = 0;
         virtual void SetInternalLights(const bool on)                                        = 0;
         virtual void SetLeftDoorsState(const bool open)                                      = 0;
         virtual void SetRightDoorsState(const bool open)                                     = 0;
-        virtual void SetCommandedPower(const types::Watts power)                             = 0;
         virtual void SetCommandedInternalTemperature(const types::DegreesFahrenheit degrees) = 0;
+        virtual types::Meters GetDistanceTraveled(void) const                                = 0;
         virtual void SetDistanceTraveled(const types::Meters distance)                       = 0;
-        virtual uint16_t GetPassengersDeboarding(void)                                       = 0;
-        virtual types::BlockId GetBlockId(void)                                              = 0;
-        virtual void SetCommandedSpeed(const types::MetersPerSecond speed)                   = 0;
-        virtual void SetAuthority(const types::Meters meters)                                = 0;
-        virtual void SetPassengersBoarding(const uint16_t passegners)                        = 0;
+        virtual uint16_t GetPassengersLeaving(void) const                                    = 0;
+        virtual uint16_t GetPassengersCount(void)                                            = 0;
+        virtual void SetCommandedSpeed(const types::MilesPerHour speed)                      = 0;
+        virtual void SetAuthority(const types::Blocks blocks)                                = 0;
+        virtual void SetPassengersBoarding(const uint16_t passengers)                        = 0;
         virtual void SetTrackPolarity(const types::Polarity polarity)                        = 0;
         virtual void SetBeaconData(const types::BeaconData &data, std::size_t &size)         = 0;
+
+        private:
+        //internal variables(model specific)
+        int passengers_on_board;
+        bool ext_light;
+        bool int_light;
+        bool right_door;
+        bool left_door;
+        types::Polarity track_pol;
+        //internal variables(calculations)
+        float max_force = 120000;//maximum force of the engine
+        types::MetersPerSecondSquared prev_acceleration = 0;
+        types::MetersPerSecondSquared acceleration = 0;
+        types::MetersPerSecondSquared max_dec_limit = -2.73;//meters per second ^2
+        types::MetersPerSecondSquared max_acc_limit = .5;//meters per second ^2
+        types::MilesPerHour velocity = 0;
+        types::MilesPerHour prev_velocity = 0;
+        types::MilesPerHour max_vel = 43.496;//mph
+        int max_passengers = 222;
+        int passengers_boarding;
+        int crew_count;
+        float train_mass = 81800;//lbs
+        float mass;
+        float force;
+        float clk_frequency;
+        float grade;//can get this from moaz or just have it
+        bool emergency_brake;
+        bool brake_failure;
+        bool engine_failure;
+        bool signal_pickup_failure;
+        types::BeaconData beacon_data;
+        types::Blocks authority;
+        types::MilesPerHour comm_speed;
+        types::TrainId train_id;
+        types::Meters distance_traveled;
+        types::DegreesFahrenheit int_temp;
+        std::string station_announcement;
+        double service_brake;
+        types::Watts power;
 };
 
 } // namespace train_model
--- src/modules/train_model/src/train_model.cc
+++ src/modules/train_model/src/train_model.cc
@@ -1,1 +1,172 @@
 #include "train_model.h"
+#include <random>
+#include <iostream>
+
+namespace train_model
+{
+train_model::TrainModel::TrainModel()
+{
+
+}
+        void TrainModel::SetTrainId(const types::TrainId train)
+        {
+            TrainModel::train_id = train;
+        }
+        types::TrainId TrainModel::GetTrainId(void) const
+        {
+            return TrainModel::train_id;
+        }
+        void TrainModel::SetEmergencyBrake(const bool emergency_brake)
+        {
+            TrainModel::emergency_brake = emergency_brake;
+        }
+        void TrainModel::SetEngineFailure(const bool engine_failure)
+        {
+            TrainModel::engine_failure = engine_failure;
+        }
+        void TrainModel::SetBrakeFailure(const bool brake_failure)
+        {
+            TrainModel::brake_failure = brake_failure;
+        }
+        void TrainModel::SetSignalPickupFailure(const bool signal_pickup_failure)
+        {
+            TrainModel::signal_pickup_failure = signal_pickup_failure;
+        }
+        bool TrainModel::GetBrakeFailure() const
+        {
+            return brake_failure;
+        }
+        bool TrainModel::GetEngineFailure() const
+        {
+            return engine_failure;
+        }
+        bool TrainModel::GetSignalPickupFailure() const
+        {
+            return signal_pickup_failure;
+        }
+        std::size_t TrainModel::GetBeaconData(types::BeaconData &data) const
+        {
+            //IDK WHAT TO DO HERE
+        }
+        types::Blocks TrainModel::GetAuthority(void) const
+        {
+            return TrainModel::authority;
+        }
+        void TrainModel::SetCommandedSpeed(types::MilesPerHour speed)
+        {
+            TrainModel::comm_speed = speed;
+        }
+        types::MilesPerHour TrainModel::GetCommandedSpeed(void) const
+        {
+            return TrainModel::comm_speed;
+        }
+        types::MetersPerSecond TrainModel::GetActualSpeed(void) const
+        {
+            return TrainModel::velocity;
+        }
+        bool TrainModel::GetEngineFailure(void) const
+        {
+            return TrainModel::engine_failure;
+        }
+        bool TrainModel::GetSignalPickupFailure(void) const
+        {
+            return TrainModel::signal_pickup_failure;
+        }
+        types::Watts TrainModel::GetActualPower(void) const
+        {
+            return TrainModel::power;
+        }
+        void TrainModel::SetActualPower(types::Watts watts)
+        {
+            TrainModel::power = watts;
+        }
+        types::DegreesFahrenheit TrainModel::GetActualInternalTemperature(void) const
+        {
+            return TrainModel::int_temp;
+        }
+        types::Polarity TrainModel::GetTrackPolarity(void) const
+        {
+            return TrainModel::track_pol;
+        }
+        void TrainModel::SetStationAnnouncement(const std::string announcement)
+        {
+            TrainModel::station_announcement = announcement;
+        }
+        void TrainModel::SetGrade(const float grade)
+        {
+            TrainModel::grade = grade;
+        }
+        void TrainModel::SetBrake(const double brake)
+        {
+            TrainModel::service_brake = brake;
+        }
+        void TrainModel::SetHeadlights(const bool on)
+        {
+            TrainModel::ext_light = on;
+        }
+        void TrainModel::SetInternalLights(const bool on)
+        {
+            TrainModel::int_light = on;
+        }
+        void TrainModel::SetLeftDoorsState(const bool open)
+        {
+            TrainModel::left_door = open;
+        }
+        void TrainModel::SetRightDoorsState(const bool open)
+        {
+            TrainModel::right_door = open;
+        }
+        void TrainModel::SetCommandedInternalTemperature(const types::DegreesFahrenheit degrees)
+        {
+            TrainModel::int_temp = degrees;
+        }
+        types::Meters TrainModel::GetDistanceTraveled(void) const
+        {
+            return TrainModel::distance_traveled;
+        }
+        void TrainModel::SetDistanceTraveled(const types::Meters distance)
+        {
+            TrainModel::distance_traveled = distance;
+        }
+        uint16_t TrainModel::GetPassengersLeaving(void) const
+        {
+            //generate random number within bounds for leaving
+            std::random_device              rd;              // Seed
+            std::mt19937                    gen(rd());       // Mersenne Twister engine
+            std::uniform_int_distribution<> dis(0, passengers_on_board); // Uniform distribution between 0 and board
+
+            // Generate a random number for passengers leaving
+            uint16_t randomNumber = dis(gen);
+
+            return randomNumber;
+        }
+        void TrainModel::SetPassengersBoarding(const uint16_t passengers)
+        {
+            TrainModel::passengers_boarding = passengers;
+        }
+        uint16_t TrainModel::GetPassengersCount(void)
+        {
+            //adds new passengers
+            TrainModel::passengers_on_board += passengers_boarding;
+            //removes old passengers
+            TrainModel::passengers_on_board -= TrainModel::GetPassengersLeaving();
+            //returns passengers on board (should be called and updated only when at station)
+            return TrainModel::passengers_on_board;
+        }
+        void TrainModel::SetCommandedSpeed(const types::MetersPerSecond speed)
+        {
+            TrainModel::comm_speed = speed;
+        }
+        void TrainModel::SetAuthority(const types::Blocks blocks)
+        {
+            TrainModel::authority = blocks;
+        }
+        void TrainModel::SetTrackPolarity(const types::Polarity polarity)
+        {
+            TrainModel::track_pol = polarity;
+        }
+        void TrainModel::SetBeaconData(const types::BeaconData &data, std::size_t &size)
+        {
+            TrainModel::beacon_data = data;
+        }
+}
