8e96f32ee06a7c66f40a861929f20e78b5f1bdac
ChibiDemon <naa135@pitt.edu>
2024-10-26 17:39:42 -0400

    NNF-182 Removed cout statements in backend

--- src/modules/train_controller/src/train_controller.cc
+++ src/modules/train_controller/src/train_controller.cc
@@ -301,7 +301,6 @@ void SoftwareTrainController::CalculateCommandedPower()
     //Checking if Current Train Velocity is greater than Setpoint speed
     else if (current_speed_ > setpoint_speed)
     {
-        std::cout << "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
         integral_sum_ = 0;
 
         commanded_power_ = 0;
@@ -314,14 +313,12 @@ void SoftwareTrainController::CalculateCommandedPower()
     //Checking if Service brake is on
     else if (service_brake_percentage_ > 0)
     {
-        std::cout << "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB";
         integral_sum_    = 0;
         commanded_power_ = 0;
     }
     //Normal power calculation
     else
     {
-        std::cout << "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC";
         commanded_power_ = kp_term + ki_term;
 
         if (commanded_power_ > max_power_)
