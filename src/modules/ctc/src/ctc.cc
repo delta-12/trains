c90ff914a3b6e32f1cdfe099b8dc2346aa5d9c9a
delta-12 <btesq235@gmail.com>
2024-10-31 16:25:11 -0400

    NNF-162 ctc stubs

--- src/modules/ctc/src/ctc.cc
+++ src/modules/ctc/src/ctc.cc
@@ -17,6 +17,28 @@
 namespace ctc
 {
 
+void Ctc::SetTrackLayout(const std::vector<types::Block> &blocks)
+{
+}
+
+void Ctc::SetSchedule(const types::TrainId train, const std::vector<DestinationAndArrivalTime> &schedule)
+{
+}
+
+void Ctc::SetManualMode(void)
+{
+}
+
+types::Error Ctc::SetBlockStates(const types::TrackId track, const std::vector<types::BlockState> &block_states)
+{
+    return types::ERROR_INVALID_TRACK;
+}
+
+std::vector<types::TrackCircuitData> Ctc::GetSuggestedSpeedsAndAuthorities(void) const
+{
+    return std::vector<types::TrackCircuitData>();
+}
+
 } // namespace ctc
 
 #endif // TRAINS_SRC_MODULES_CTC_INC_CTC_CC
