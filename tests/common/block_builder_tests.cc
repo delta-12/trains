e09b19533bb5bf33f5191fdca533f0457bcfb4ef
Son Vu <truongson.vu72@gmail.com>
2024-11-01 17:06:48 -0400

    NNF-221 MinuteToMiliseconds test case added

--- tests/common/block_builder_tests.cc
+++ tests/common/block_builder_tests.cc
@@ -9,6 +9,7 @@
 #include "csv_parser.h"
 #include "block_builder.h"
 #include "types.h"
+#include "convert.h"
 
 TEST(BlockBuilderTests, GetSize)
 {
@@ -167,4 +168,19 @@ TEST(BlockBuilderTests, GreenLineV4)
     ASSERT_EQ(block.has_station, true);
     ASSERT_EQ(block.station_name, "Central");
     ASSERT_EQ(block.underground, true);
+}
+
+TEST(BlockBuilderTests, MinuteToMiliSecond)
+{
+    std::string               minute     = "2.5";
+    std::chrono::milliseconds milisecond = convert::ConvertMinuteToMiliseconds(minute);
+    ASSERT_EQ(milisecond.count(), 150000);
+
+    minute     = "2.7";
+    milisecond = convert::ConvertMinuteToMiliseconds(minute);
+    ASSERT_EQ(milisecond.count(), 162000);
+
+    minute     = "3.2";
+    milisecond = convert::ConvertMinuteToMiliseconds(minute);
+    ASSERT_EQ(milisecond.count(), 192000);
 }
