e88feff91ca8aee993f722baabedd26e4a47c30f
Charles Prather <cjp103@pitt.edu>
2024-11-09 15:09:28 -0500

    NNF 232 - 1 L

--- src/modules/train_model/inc/train_model.h
+++ src/modules/train_model/inc/train_model.h
@@ -40,7 +40,7 @@ class TrainModel
         virtual types::Polarity GetTrackPolarity(void) const                      = 0;
         virtual uint16_t GetPassengersDeboarding(void)                            = 0;
         virtual types::TrainId GetTrainId(void) const                             = 0;
-        virtual types::Meters GetDistanceTravelled(void) const                     = 0;
+        virtual types::Meters GetDistanceTraveled(void) const                     = 0;
 
         virtual void SetStationAnnouncement(const std::string &announcement)                 = 0;
         virtual void SetGrade(const double grade)                                            = 0;
@@ -106,7 +106,7 @@ class SoftwareTrainModel : public TrainModel
         types::Watts GetActualPower(void) const;
         types::DegreesFahrenheit GetActualInternalTemperature(void) const;
         types::Polarity GetTrackPolarity(void) const;
-        types::Meters GetDistanceTravelled(void) const;
+        types::Meters GetDistanceTraveled(void) const;
         uint16_t GetPassengersDeboarding(void);
         uint16_t GetPassengersCount(void);
 
