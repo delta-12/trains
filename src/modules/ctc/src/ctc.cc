2bb911558b9395ce4dc71c5d1bc87169cd32e851
Son Vu <truongson.vu72@gmail.com>
2024-11-12 21:50:05 -0500

    NNF-247 uncrust

--- src/modules/ctc/src/ctc.cc
+++ src/modules/ctc/src/ctc.cc
@@ -273,11 +273,13 @@ std::vector<types::BlockId> Ctc::GetFailureBlocks(void) const
     return failure_blocks_;
 }
 
-std::vector<types::Block> Ctc::GetBlocks(void) const {
+std::vector<types::Block> Ctc::GetBlocks(void) const
+{
     return blocks_;
 }
 
-std::vector<ctc::Train> Ctc::GetTrains(void) const {
+std::vector<ctc::Train> Ctc::GetTrains(void) const
+{
     return train_schedules_;
 }
 
