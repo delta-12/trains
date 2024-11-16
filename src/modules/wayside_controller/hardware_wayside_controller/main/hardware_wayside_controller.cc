7e29d26946527c078cb1d7e0fdd8052703f1a50d
delta-12 <btesq235@gmail.com>
2024-11-15 21:17:01 -0500

    NNF-254 cleanup hw wc cmake paths

--- src/modules/wayside_controller/hardware_wayside_controller/main/hardware_wayside_controller.cc
+++ src/modules/wayside_controller/hardware_wayside_controller/main/hardware_wayside_controller.cc
@@ -1,14 +1,4 @@
-#include "wayside_controller.h"
-#include "green_line.h"
-
-wayside_controller::Error GetInput(const wayside_controller::InputId input, wayside_controller::IoSignal &signal);
-
 extern "C" void app_main(void)
 {
 
-}
-
-wayside_controller::Error GetInput(const wayside_controller::InputId input, wayside_controller::IoSignal &signal)
-{
-    return wayside_controller::Error::ERROR_INVALID_INPUT;
 }
