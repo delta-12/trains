323de1e3db040369a462873170d7b9c8ed987c84
delta-12 <btesq235@gmail.com>
2024-11-19 19:39:49 -0500

    NNF-254 only apply uncrustify and all warnings, warnings as errors to internal sources

--- tools/uncrustify/uncrustify.cmake
+++ tools/uncrustify/uncrustify.cmake
@@ -16,7 +16,7 @@ endif()
 
 message(STATUS "Adding files to Uncrustify")
 set(UNCRUSTIFY_SOURCES ${TRAINS_SRCS} ${TEST_SRCS})
-foreach(INC_DIR ${INC_DIRS})
+foreach(INC_DIR ${INTERNAL_INC_DIRS})
     file(GLOB INC_DIR_H_SRCS LIST_DIRECTORIES false CONFIGURE_DEPENDS
         "${INC_DIR}/*.h"
     )
