ab4efb54101c6966f907255d0dee5a60752cd25d
delta-12 <btesq235@gmail.com>
2024-11-20 23:59:43 -0500

    NNF-255 refactor CTC callbacks

--- src/modules/ctc/inc/ctc_callback_handler.h
+++ src/modules/ctc/inc/ctc_callback_handler.h
@@ -6,23 +6,16 @@
 #ifndef TRAINS_SRC_MODULES_CTC_INC_CTC_CALLBACK_HANDLER_H
 #define TRAINS_SRC_MODULES_CTC_INC_CTC_CALLBACK_HANDLER_H
 
-#include <iostream>
-
-#include "launcher.h"
-#include "ctc.h"
 #include "channel.h"
+#include "ctc.h"
+#include "launcher.h"
 
 namespace ctc
 {
-void handle_manual_dispatch(slint::ComponentHandle<ui::CtcUi> &ctc_ui, ctc::Ctc& ctc, Channel<std::string> &channel);
-void handle_set_occupancy(slint::ComponentHandle<ui::CtcUi> &ctc_ui, ctc::Ctc& ctc,
-                          std::shared_ptr<slint::VectorModel<std::shared_ptr<slint::Model<slint::StandardListViewItem>>>> &received_train_schedules,
-                          std::shared_ptr<slint::VectorModel<std::shared_ptr<slint::Model<slint::StandardListViewItem>>>> &block_data_model,
-                          Channel<std::string> &channel);
-void handle_set_block_states(ctc::Ctc& ctc,
-                             std::shared_ptr<slint::VectorModel<std::shared_ptr<slint::Model<slint::StandardListViewItem>>>> &received_train_schedules,
-                             std::shared_ptr<slint::VectorModel<std::shared_ptr<slint::Model<slint::StandardListViewItem>>>> &block_data_model,
-                             Channel<std::vector<types::BlockState>> &channel);
+
+void setup_ui(ui::CtcUi &ctc_ui);
+void backend_handler(ctc::Ctc &ctc_office);
+
 }
 
 #endif // TRAINS_SRC_MODULES_CTC_INC_CTC_CALLBACK_HANDLER_H
