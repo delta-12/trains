4b118db5c5a8f95b05393b9303f6cdbbd2843beb
delta-12 <btesq235@gmail.com>
2024-11-20 22:48:32 -0500

    NNF-254 test instantiations in worker thread

--- src/main.cc
+++ src/main.cc
@@ -16,36 +16,26 @@ int main(void)
 {
     std::atomic_bool running(true);
     simulator::Simulator world;
-    auto                 launcher_ui           = ui::Launcher::create();
-    auto                 ctc_ui                = ui::CtcUi::create();
-    auto                 wayside_controller_ui = ui::WaysideControllerUi::create();
-    auto                 track_model_ui        = ui::TrackModelUi::create();
-    auto                 train_model_ui        = ui::TrainModelUi::create();
-    auto                 train_controller_ui   = ui::TrainControllerUi::create();
+    auto launcher_ui = ui::Launcher::create();
+    auto ctc_ui = ui::CtcUi::create();
+    auto wayside_controller_ui = ui::WaysideControllerUi::create();
+    auto track_model_ui = ui::TrackModelUi::create();
+    auto train_model_ui = ui::TrainModelUi::create();
+    auto train_controller_ui = ui::TrainControllerUi::create();
 
     launcher_ui->on_launch_ctc_window([&]
-    {
-        ctc_ui->show();
-    });
+                                      { ctc_ui->show(); });
     launcher_ui->on_launch_wayside_controller_window([&]
-    {
-        wayside_controller_ui->show();
-    });
+                                                     { wayside_controller_ui->show(); });
     launcher_ui->on_launch_track_model_window([&]
-    {
-        track_model_ui->show();
-    });
+                                              { track_model_ui->show(); });
     launcher_ui->on_launch_train_model_window([&]
-    {
-        train_model_ui->show();
-    });
+                                              { train_model_ui->show(); });
     launcher_ui->on_launch_train_controller_window([&]
-    {
-        train_controller_ui->show();
-    });
+                                                   { train_controller_ui->show(); });
 
     std::thread worker_thread([&]
-    {
+                              {
         std::filesystem::path           base_path = std::filesystem::current_path();
         std::filesystem::path           path      = base_path / "tests" / "common" / "test_csv" / "green_line_path.csv";
         std::filesystem::path           path2     = base_path / "tests" / "common" / "test_csv" / "green_line_track_layout.csv";
@@ -54,16 +44,33 @@ int main(void)
         BlockBuilder                    bb(parser.GetRecords(), RecordType::RECORDTYPE_TRACK_LAYOUT);
         BlockBuilder                    bb2(parser2.GetRecords(), RecordType::RECORDTYPE_TRACK_LAYOUT);
         
+        // Create clock
         std::shared_ptr<TickSource> tick_source = std::make_shared<TickSource>();
-        std::shared_ptr<train_model::TrainModel> train = std::make_shared<train_model::SoftwareTrainModel>(tick_source);
-        std::shared_ptr<track_model::SoftwareTrackModel> track = std::make_shared<track_model::SoftwareTrackModel>();
 
+        // Create CTC
+        ctc::Ctc              ctc_office;
+        std::filesystem::path path      = base_path / "tests" / "common" / "test_csv" / "green_line_schedule.csv";
+        ctc_office.SetScheduleFilePath(path);
+        ctc_office.SetTrackLayout();
+
+        // Create train
+        std::shared_ptr<train_model::TrainModel> train = std::make_shared<train_model::SoftwareTrainModel>(tick_source);
+
+        // Create track
+        std::shared_ptr<track_model::SoftwareTrackModel> track = std::make_shared<track_model::SoftwareTrackModel>();
         track->SetTrackLayout(types::TrackId::TRACKID_GREEN, bb.GetBlocks(), bb2.GetBlocks());
 
+        // Add track and train to simulator
         world.AddTrackModel(track);
         world.AddTrainModel(track->GetTrackId(), train);
         world.Update();
-    });
+
+        // TODO instantiate wayside handler and controller handler
+
+        while (running.get())
+        {
+            // TODO run simulator update, controller handler update, and wayside update
+        } });
 
     launcher_ui->run();
     running.store(false);
