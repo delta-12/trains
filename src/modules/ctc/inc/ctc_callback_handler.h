7699cb69c3e05c8d0b02681be3154af83a440d55
Son Vu <97687998+Sonnvu@users.noreply.github.com>
2024-12-03 17:48:11 -0500

    Nnf 255 multiple destination and return yard (#66)
    * NNF-255 Dispatch Train To Multiple Destination, Back To Yard After
    
    * NNF-255 Callbacks stuff
    
    * NNF-255 Uncrustify
    
    * NNF-255 Handle random crashes
    
    * NNF-255 Suppressed Unused Function
    
    * NNF-255-Multiple-Destination-And-Return-Yard
    
    * NNF-255 RAWR
    
    * NNF-255 Uncrustify
    
    * NNF-255 refactor CTC callbacks
    
    * NNF-255 Merge Conflicts
    
    * NNF-255 Bug fix, Ctc Callback Handler and Threads
    
    * NNF-255 Add Ctc backend tests
    
    * NNF-255 Address PR Comments
    
    * NNF-255 Test Coverage
    
    * NNF-255 Sonarqb suggestion fix
    
    * NNF-255 Sonarqb suggestion fix
    
    * NNF-255 Handle file path doesn't exist
    
    * NNF-255 Handle Path Not Found
    
    * NNF-255 sonar ignore handlers
    
    ---------
    
    Co-authored-by: delta-12 <btesq235@gmail.com>

---
+++ src/modules/ctc/inc/ctc_callback_handler.h
@@ -0,0 +1,21 @@
+/*****************************************************************************
+* @file ctc_callback_handler.h
+*
+* @brief Implements the CTC UI callbacks.
+*****************************************************************************/
+#ifndef TRAINS_SRC_MODULES_CTC_INC_CTC_CALLBACK_HANDLER_H
+#define TRAINS_SRC_MODULES_CTC_INC_CTC_CALLBACK_HANDLER_H
+
+#include "channel.h"
+#include "ctc.h"
+#include "launcher.h"
+
+namespace ctc
+{
+
+void setup_ui(slint::ComponentHandle<ui::CtcUi> &ctc_ui, ctc::Ctc &ctc);
+void backend_handler(ctc::Ctc &ctc_office, slint::ComponentHandle<ui::CtcUi> &ctc_ui);
+
+}
+
+#endif // TRAINS_SRC_MODULES_CTC_INC_CTC_CALLBACK_HANDLER_H
--- tests/modules/ctc/ctc_backend_tests.cc
+++ tests/modules/ctc/ctc_backend_tests.cc
@@ -10,11 +10,7 @@
 
 TEST(CtcBackEndTest, SetTrackLayout)
 {
-    ctc::Ctc              ctc;
-    std::filesystem::path base_path = std::filesystem::current_path();
-    std::filesystem::path path      = base_path / ".." / "tests" / "common" / "test_csv" / "green_line_schedule.csv";
-    ctc.SetScheduleFilePath(path);
-    ctc.SetTrackLayout();
+    ctc::Ctc ctc(types::TrackId::TRACKID_GREEN);
 
     std::vector<types::Block> blocks = ctc.GetBlocks();
     ASSERT_EQ(blocks.size(), 151);
@@ -36,11 +32,7 @@ TEST(CtcBackEndTest, SetTrackLayout)
 
 TEST(CtcBackEndTest, SetStations)
 {
-    ctc::Ctc              ctc;
-    std::filesystem::path base_path = std::filesystem::current_path();
-    std::filesystem::path path      = base_path / ".." / "tests" / "common" / "test_csv" / "green_line_schedule.csv";
-    ctc.SetScheduleFilePath(path);
-    ctc.SetTrackLayout();
+    ctc::Ctc ctc(types::TrackId::TRACKID_GREEN);
 
     ASSERT_EQ(ctc.GetNumStation(), 18);
 
@@ -58,38 +50,83 @@ TEST(CtcBackEndTest, SetManualMode)
     ASSERT_EQ(ctc.GetOperationMode(), ctc::MANUAL_MODE);
 }
 
+TEST(CtcBackEndTest, DispatchMultipleDestination)
+{
+    ctc::Ctc ctc(types::TrackId::TRACKID_GREEN);
+    // Dispatch Train regardless of ID
+    ctc.ManualDispatch(1, 70);
+    ctc::Train   train(0);
+    types::Error error = ctc.GetTrainById(1, train);
+    ASSERT_EQ(error, types::Error::ERROR_NONE);
+    ASSERT_EQ(train.train_id, 1);
+    ASSERT_EQ(ctc.GetNumTrains(), 1);
+    ASSERT_EQ(train.destination_list.size(), 1);
+
+    // Dispatch Train no matching ID -> new train dispatched making sure train id will always be in increment order
+    ctc.ManualDispatch(0, 80);
+    ASSERT_EQ(ctc.GetNumTrains(), 2);
+
+    // Add another Destinatino to train 1
+    ctc.ManualDispatch(1, 90);
+    ASSERT_EQ(ctc.GetNumTrains(), 2);
+
+    error = ctc.GetTrainById(1, train);
+    ASSERT_EQ(train.destination_list.size(), 2);
+    ASSERT_EQ(train.destination_list[0].destination, 70);
+    ASSERT_EQ(train.destination_list[1].destination, 90);
+}
+
+TEST(CtcBackEndTest, Authority)
+{
+    ctc::Ctc ctc(types::TrackId::TRACKID_GREEN);
+    ctc.ManualDispatch(1, 70);
+    ASSERT_EQ(ctc.GetTrainAuthority(1), 8);
+}
+
+TEST(CtcBackEndTest, ReturnToYard)
+{
+    ctc::Ctc ctc(types::TrackId::TRACKID_GREEN);
+    ctc.ManualDispatch(1, 0);
+    ctc::Train train1;
+    ctc.GetTrainById(1, train1);
+    ASSERT_EQ(train1.authority.size(), 171);
+
+    std::vector<types::BlockId> route = ctc.GetRoute(70, 75);
+    ASSERT_EQ(route.size(), 5);
+}
+
 TEST(CtcBackEndTest, ManualDispatchToBlock)
 {
-    ctc::Ctc              ctc;
-    std::filesystem::path base_path = std::filesystem::current_path();
-    std::filesystem::path path      = base_path / ".." / "tests" / "common" / "test_csv" / "green_line_schedule.csv";
-    ctc.SetScheduleFilePath(path);
-    ctc.SetTrackLayout();
+    ctc::Ctc ctc(types::TrackId::TRACKID_GREEN);
 
     ASSERT_EQ(ctc.GetNumStation(), 18);
-    ctc.ManualDispatch(105);
+    ctc.ManualDispatch(1, 105);
 
     std::vector<ctc::Train> trains = ctc.GetTrains();
     ASSERT_EQ(trains.size(), 1);
 
-    ctc::Train train1 = ctc.GetTrainById(1);
+    ctc::Train train1;
+    ctc.GetTrainById(1, train1);
     ASSERT_EQ(train1.authority.front(), 63);
 
+    types::Error error = ctc.UpdateSuggestedSpeedAndAuthority(2);
+    ASSERT_EQ(error, types::Error::ERROR_INVALID_TRAIN);
+
     ctc.UpdateSuggestedSpeedAndAuthority(1);
-    train1 = ctc.GetTrainById(1);
+    ctc.GetTrainById(1, train1);
     ASSERT_EQ(train1.authority.front(), 64);
     ASSERT_EQ(train1.suggested_speed, 19);
 
     ctc.UpdateSuggestedSpeedAndAuthority(1);
-    train1 = ctc.GetTrainById(1);
+    ctc.GetTrainById(1, train1);
     ASSERT_EQ(train1.authority.front(), 65);
 
     ctc.UpdateSuggestedSpeedAndAuthority(1);
-    train1 = ctc.GetTrainById(1);
+    ctc.GetTrainById(1, train1);
     ASSERT_EQ(train1.authority.front(), 66);
 
     ctc.UpdateSuggestedSpeedAndAuthority(1);
-    train1 = ctc.GetTrainById(1);
+    ctc.GetTrainById(1, train1);
     ASSERT_EQ(train1.authority.front(), 67);
 
     std::vector<types::BlockId> authority;
@@ -103,11 +140,7 @@ TEST(CtcBackEndTest, ManualDispatchToBlock)
 
 TEST(CtcBackEndTest, SetBlockStates)
 {
-    ctc::Ctc              ctc;
-    std::filesystem::path base_path = std::filesystem::current_path();
-    std::filesystem::path path      = base_path / ".." / "tests" / "common" / "test_csv" / "green_line_schedule.csv";
-    ctc.SetScheduleFilePath(path);
-    ctc.SetTrackLayout();
+    ctc::Ctc ctc(types::TrackId::TRACKID_GREEN);
 
     std::vector<types::BlockState> block_states;
     types::BlockState              block_state_1(63, true, false);
@@ -120,19 +153,20 @@ TEST(CtcBackEndTest, SetBlockStates)
     ASSERT_EQ(ctc.GetBlockById(63).occupied, true);
     ASSERT_EQ(ctc.GetBlockById(70).occupied, true);
     ASSERT_EQ(ctc.GetFailureBlocks()[0], 70);
+
+    block_states.emplace_back(151, true, false);
+    types::Error error = ctc.SetBlockStates(types::TrackId::TRACKID_GREEN, block_states);
+    ASSERT_EQ(error, types::Error::ERROR_INVALID_BLOCK);
 }
 
 TEST(CtcBackEndTest, TrainReceiveBlockOccupancy)
 {
-    ctc::Ctc              ctc;
-    std::filesystem::path base_path = std::filesystem::current_path();
-    std::filesystem::path path      = base_path / ".." / "tests" / "common" / "test_csv" / "green_line_schedule.csv";
-    ctc.SetScheduleFilePath(path);
-    ctc.SetTrackLayout();
+    ctc::Ctc ctc(types::TrackId::TRACKID_GREEN);
 
-    ctc.ManualDispatch(105);
+    ctc.ManualDispatch(1, 105);
 
-    ctc::Train train1 = ctc.GetTrainById(1);
+    ctc::Train train1;
+    ctc.GetTrainById(1, train1);
     ASSERT_EQ(train1.authority.front(), 63);
     ASSERT_EQ(train1.current_position, 0);
 
@@ -141,21 +175,48 @@ TEST(CtcBackEndTest, TrainReceiveBlockOccupancy)
     block_states.push_back(block_state_1);
     ctc.SetBlockStates(types::TrackId::TRACKID_GREEN, block_states);
 
-    train1 = ctc.GetTrainById(1);
+    ctc.GetTrainById(1, train1);
     ASSERT_EQ(train1.authority.front(), 64);
-    ASSERT_EQ(train1.current_position, 63);
-    ASSERT_EQ(train1.suggested_speed, 19);
+    ASSERT_EQ(ctc.GetTrainCurrentPosition(1), 63);
+    ASSERT_EQ(ctc.GetTrainSuggestedSpeed(1), 19);
+}
+
+TEST(CtcBackEndTest, ManualDispatchToMultipleBlock)
+{
+    ctc::Ctc ctc(types::TrackId::TRACKID_GREEN);
+    ctc.ManualDispatch(1, 67);
+    ctc.ManualDispatch(1, 70);
+    ctc::Train train1;
+    ctc.GetTrainById(1, train1);
+    ASSERT_EQ(train1.current_position, 0);      // Sitting in yard
+    ASSERT_EQ(train1.authority.size(), 5);      // Authority = 5 blocks to get to block 67
+    ASSERT_EQ(train1.destination_list[CTC_TRAIN_CURRENT_DESTINATION].destination, 67);
+    ASSERT_EQ(train1.destination_list[1].destination, 70);
+
+    ctc.SetBlockStates(types::TrackId::TRACKID_GREEN, { types::BlockState(63, true, false) });
+    ctc.SetBlockStates(types::TrackId::TRACKID_GREEN, { types::BlockState(64, true, false) });
+    ctc.SetBlockStates(types::TrackId::TRACKID_GREEN, { types::BlockState(65, true, false) });
+    ctc.SetBlockStates(types::TrackId::TRACKID_GREEN, { types::BlockState(66, true, false) });
+    ctc.SetBlockStates(types::TrackId::TRACKID_GREEN, { types::BlockState(67, true, false) });
+    ctc.GetTrainById(1, train1);
+    ASSERT_EQ(train1.current_position, 67);
+    ASSERT_EQ(train1.authority.size(), 3);
+    ASSERT_EQ(train1.destination_list[CTC_TRAIN_CURRENT_DESTINATION].destination, 70);
+
+    ctc.SetBlockStates(types::TrackId::TRACKID_GREEN, { types::BlockState(68, true, false) });
+    ctc.SetBlockStates(types::TrackId::TRACKID_GREEN, { types::BlockState(69, true, false) });
+    ctc.SetBlockStates(types::TrackId::TRACKID_GREEN, { types::BlockState(70, true, false) });
+    ctc.GetTrainById(1, train1);
+    ASSERT_EQ(train1.current_position, 70);
+    ASSERT_EQ(train1.authority.size(), 163);
+    ASSERT_EQ(train1.destination_list[CTC_TRAIN_CURRENT_DESTINATION].destination, 0);
 }
 
 TEST(CtcBackEndTest, GetSuggestedSpeedAndAuthorities)
 {
-    ctc::Ctc              ctc;
-    std::filesystem::path base_path = std::filesystem::current_path();
-    std::filesystem::path path      = base_path / ".." / "tests" / "common" / "test_csv" / "green_line_schedule.csv";
-    ctc.SetScheduleFilePath(path);
-    ctc.SetTrackLayout();
+    ctc::Ctc ctc(types::TrackId::TRACKID_GREEN);
     // Dispatch train to block 70
-    ctc.ManualDispatch(70);
+    ctc.ManualDispatch(1, 70);
 
     // CTC Received Block Occupancy at 63
     std::vector<types::BlockState> block_states = { types::BlockState(63, true, false) };
@@ -164,6 +225,7 @@ TEST(CtcBackEndTest, GetSuggestedSpeedAndAuthorities)
     std::vector<types::TrackCircuitData> track_circuit_data = ctc.GetSuggestedSpeedsAndAuthorities();
     ASSERT_EQ(track_circuit_data.size(), 1);
     types::TrackCircuitData data = track_circuit_data[0];
+    ASSERT_EQ(data.track, types::TrackId::TRACKID_GREEN);
     ASSERT_EQ(data.authority, 7);
     ASSERT_EQ(data.block, 63);
     ASSERT_EQ(data.speed, 19);
@@ -174,6 +236,7 @@ TEST(CtcBackEndTest, GetSuggestedSpeedAndAuthorities)
 
     track_circuit_data = ctc.GetSuggestedSpeedsAndAuthorities();
     data               = track_circuit_data[0];
+    ASSERT_EQ(data.track, types::TrackId::TRACKID_GREEN);
     ASSERT_EQ(data.authority, 6);
     ASSERT_EQ(data.block, 64);
     ASSERT_EQ(data.speed, 19);
